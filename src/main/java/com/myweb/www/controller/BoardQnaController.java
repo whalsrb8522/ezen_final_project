d18fb1dff3d490b87afcf5bef628e09c6ca0f106
hyunjoo <hjstal@naver.com>
2023-06-28 15:13:28 +0900

    branch 테스트

---
+++ src/main/java/com/myweb/www/controller/BoardQnaController.java
@@ -0,0 +1,14 @@
+package com.myweb.www.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board_qna/*")
+public class BoardQnaController {
+
+	@GetMapping("list")
+	public void list() {}
+	
+}
