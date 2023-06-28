0dfb12ab613db121b1400d7ee000453cff5940d7
whalsrb8522 <whalsrb8522@gmail.com>
2023-06-28 10:24:39 +0900

    first commit

---
+++ src/main/java/com/myweb/www/HomeController.java
@@ -0,0 +1,31 @@
+package com.myweb.www;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+	@GetMapping("/")
+	public String home(Locale locale, Model model) {
+		log.info("test");
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+}
