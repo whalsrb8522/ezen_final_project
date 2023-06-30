8b36fc0528235f0f12b1a561e9b8b2b3e69cd47d
whalsrb8522 <whalsrb8522@gmail.com>
2023-06-30 13:20:19 +0900

    회원가입 페이지 작성

--- src/main/java/com/myweb/www/controller/MemberController.java
+++ src/main/java/com/myweb/www/controller/MemberController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Controller
 @RequestMapping("/member/*")
 public class MemberController {
