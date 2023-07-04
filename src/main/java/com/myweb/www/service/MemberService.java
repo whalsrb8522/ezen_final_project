cb2b0dc766d71416b8b3876e831b2d6151d876d1
amoled4 <amoled4@naver.com>
2023-07-04 14:32:58 +0900

    commit

---
+++ src/main/java/com/myweb/www/service/MemberService.java
@@ -0,0 +1,12 @@
+package com.myweb.www.service;
+
+import com.myweb.www.domain.MemberVO;
+
+public interface MemberService {
+
+	static MemberVO isMember(String m_mail, String m_pw) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
