c772fb758f63fe4845a7b532467d6a2cf657b043
whalsrb8522 <whalsrb8522@gmail.com>
2023-07-04 15:04:11 +0900

    Update chat

--- src/main/java/com/myweb/www/controller/ChatController.java
+++ src/main/java/com/myweb/www/controller/ChatController.java
@@ -4,11 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Controller
 @RequestMapping("/chat/*")
-@Slf4j
+//@Slf4j
 public class ChatController {
 	
 	@GetMapping("main")
@@ -16,4 +14,5 @@ public class ChatController {
 
 	@GetMapping("test")
 	public void test() {}
+	
 }
