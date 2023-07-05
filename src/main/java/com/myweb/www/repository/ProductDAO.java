fbe4b6cc51070e790c2658d8f077608a0d697594
amoled4 <amoled4@naver.com>
2023-07-05 15:09:43 +0900

    기능시작

---
+++ src/main/java/com/myweb/www/repository/ProductDAO.java
@@ -0,0 +1,16 @@
+package com.myweb.www.repository;
+
+import java.util.List;
+
+import com.myweb.www.domain.ProductPagingVO;
+import com.myweb.www.domain.ProductVO;
+
+public interface ProductDAO {
+
+	List<ProductVO> productist(ProductPagingVO ppvo);
+
+	List<ProductVO> productList(ProductPagingVO ppvo);
+
+	int getTotalCount(ProductPagingVO ppvo);
+
+}
