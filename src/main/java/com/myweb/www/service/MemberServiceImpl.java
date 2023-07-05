d5a9414ec976b0da1e72357a5f0c86ca94eb08e5
amoled4 <amoled4@naver.com>
2023-07-05 10:31:27 +0900

    commit

--- src/main/java/com/myweb/www/service/MemberServiceImpl.java
+++ src/main/java/com/myweb/www/service/MemberServiceImpl.java
@@ -1,8 +1,66 @@
 package com.myweb.www.service;
 
+import javax.inject.Inject;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.myweb.www.domain.MemberVO;
+import com.myweb.www.repository.MemberDAO;
+
 
 @Service
 public class MemberServiceImpl implements MemberService {
+	
+	@Inject
+	private MemberDAO mdao;
+	@Inject
+	BCryptPasswordEncoder passwordencoder;
+
+	@Override
+	public int signUp(MemberVO member) {
+		MemberVO tempMember = mdao.getMember(member.getM_mail());
+		
+		if(tempMember != null) {
+			return 0;
+		}
+		
+		if(member.getM_mail() == null || member.getM_mail().length() == 0) {
+			return 0;
+		}
+		
+		if(member.getM_pw() == null || member.getM_pw().length() == 0) {
+			return 0;
+		}
+		
+		//회원가입 진행
+		String m_pw = member.getM_pw();
+		
+		//encode(암호화) / matches(원래 비번, 암호화된 비번)
+		String encodeM_pw = passwordencoder.encode(m_pw); // 기존 패스워드 암호화
+		member.setM_pw(encodeM_pw);
+		
+		
+		int isOk = mdao.insertMember(member);
+		
+		return isOk;
+	}
+
+	@Override
+	public MemberVO isMember(String m_mail, String m_pw) {
+		
+		MemberVO member = mdao.getMember(m_mail);
+		
+		//m_mail이 일치하는 객체가 없을 경우
+		if(member == null) { return null; }
+		
+		//원래 pw와 암호화된 pw가 매치가 된다면 true
+		if(passwordencoder.matches(m_pw, member.getM_pw())) {
+			return member;
+		}else {
+			return null;
+			
+		}
+	}
 
 }
