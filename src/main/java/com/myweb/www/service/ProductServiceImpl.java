f92cc128205014a7773ea37dfcc66ea5f9ee126c
amoled4 <amoled4@naver.com>
2023-07-06 15:17:14 +0900

    register이어서

--- src/main/java/com/myweb/www/service/ProductServiceImpl.java
+++ src/main/java/com/myweb/www/service/ProductServiceImpl.java
@@ -6,14 +6,19 @@ import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
 
+import com.myweb.www.domain.ProductDTO;
+import com.myweb.www.domain.ProductImageVO;
 import com.myweb.www.domain.ProductPagingVO;
 import com.myweb.www.domain.ProductVO;
 import com.myweb.www.repository.ProductDAO;
+import com.myweb.www.repository.ProductImageDAO;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 	@Inject
 	private ProductDAO pdao;
+	@Inject
+	private ProductImageDAO pidao;
 
 	@Override
 	public List<ProductVO> productList(ProductPagingVO ppvo) {
@@ -25,6 +30,23 @@ public class ProductServiceImpl implements ProductService {
 	public int getTotalCount(ProductPagingVO ppvo) {
 		// TODO Auto-generated method stub
 		return pdao.getTotalCount(ppvo);
+	}
+
+	@Override
+	public int productRegister(ProductDTO pdto) {
+		int isOk = pdao.insertProductRegister(pdto.getPvo());
+		if(pdto.getPiList()==null) {
+			isOk*=1;
+		}else {
+			if(isOk>0 && pdto.getPiList().size()>0) {
+				int p_number = pdao.selectPnumber();
+				for(ProductImageVO pivo : pdto.getPiList()) {
+					pivo.setP_number(p_number);
+					isOk *= pidao.insertProductImage(pivo);
+				}
+			}
+		}
+		return isOk;
 	}
 	
 }
