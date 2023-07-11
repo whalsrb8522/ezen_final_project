888cdd16ea242f6c9932afefff2f501967f9f471
amoled4 <amoled4@naver.com>
2023-07-11 15:09:58 +0900

    왜 이미지 안 돼

--- src/main/java/com/myweb/www/service/ProductService.java
+++ src/main/java/com/myweb/www/service/ProductService.java
@@ -3,14 +3,17 @@ package com.myweb.www.service;
 import java.util.List;
 
 import com.myweb.www.domain.ProductDTO;
+import com.myweb.www.domain.ProductImageVO;
 import com.myweb.www.domain.ProductPagingVO;
 import com.myweb.www.domain.ProductVO;
 
 public interface ProductService {
 
-	List<ProductVO> productList(ProductPagingVO ppvo);
-
 	int getTotalCount(ProductPagingVO ppvo);
 
 	int productRegister(ProductDTO pdto);
+
+	List<ProductDTO> listFile(ProductPagingVO ppvo);
+
+	
 }
