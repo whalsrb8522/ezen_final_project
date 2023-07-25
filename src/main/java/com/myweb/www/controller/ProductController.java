79caadaf65f4f0c4b949bf0a77dfe04dd7589bc8
amoled4 <amoled4@naver.com>
2023-07-25 15:17:55 +0900

    kka

--- src/main/java/com/myweb/www/controller/ProductController.java
+++ src/main/java/com/myweb/www/controller/ProductController.java
@@ -5,11 +5,16 @@ import java.util.List;
 import java.util.Map;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.myweb.www.domain.MemberVO;
 import com.myweb.www.domain.ProductDTO;
 import com.myweb.www.domain.ProductImageVO;
 import com.myweb.www.domain.ProductLikeVO;
@@ -58,21 +64,41 @@ public class ProductController {
 		if(files[0].getSize()>0) {
 			piList = pihd.uploadFiles(files);
 		}
-		ProductDTO pdto = new ProductDTO(pvo, piList);
+		ProductDTO pdto = new ProductDTO(pvo, null, piList);
 		int isOk = psv.productRegister(pdto);
 		rAttr.addFlashAttribute("isOk", isOk);
 		return "redirect:/product/list";
 	}
 	
 	@GetMapping("/detail")
-	public void detail(@RequestParam("p_number")int p_number, Model m) {
+	public void detail(@RequestParam("p_number")int p_number, Model m, HttpSession ses) {
+		MemberVO sesMvo = (MemberVO) ses.getAttribute("ses");
+		ProductDTO pdto = new ProductDTO();
 		psv.readCount(p_number);
-		ProductDTO pdto = psv.detail(p_number);
+		if (sesMvo == null) {
+			pdto = psv.detail(p_number);
+		} else {
+			pdto = psv.detail(p_number, sesMvo.getM_number());
+		}
+		log.info(">>> pdto = " + pdto.toString());
 		m.addAttribute("pdto", pdto);
 	}
 	
+	@PostMapping(value = "/status", consumes = "application/json")
+	public ResponseEntity<Integer> status(@RequestBody Map<String, Integer> data) {
+		ProductVO pvo = new ProductVO();
+		for (String key : data.keySet()) {
+			if(key.equals("p_number")) {
+				pvo.setP_number(data.get(key));
+			} else if(key.equals("p_status")) {
+				pvo.setP_status(data.get(key));
+			}
+		}
+		int isOk = psv.updateStatus(pvo);
+		return ResponseEntity.ok(isOk);
+	}
 	
-	@PostMapping(value = "/detail", consumes = "application/json")
+	@PostMapping(value = "/like", consumes = "application/json")
 	public ResponseEntity<String> postDetail(@RequestBody Map<String, Integer> data) {
 		ProductLikeVO plvo = new ProductLikeVO();
 			
@@ -83,7 +109,6 @@ public class ProductController {
 				plvo.setM_number(data.get(key));
 			}
 		}
-		
 		boolean isLiked = psv.insertLikeMember(plvo);
 		if (isLiked) {
 	        return ResponseEntity.ok("liked"); // 찜되었다는 응답
@@ -98,8 +123,22 @@ public class ProductController {
 		m.addAttribute("pdto", pdto);
 	}
 	
-	
-	
-	
-	
+	@PostMapping("/modify")
+	public String postModify(RedirectAttributes rAttr, ProductVO pvo, @RequestParam(name="files")MultipartFile[] files) {
+		List<ProductImageVO> piList = null;
+		if(files[0].getSize()>0) {
+			piList = pihd.uploadFiles(files);
+		}
+		ProductDTO pdto = new ProductDTO(pvo, null, piList);
+		int isOk = psv.updateModify(pdto);
+		log.info(">>> 글 수정 > "+(isOk>0?"성공":"실패"));
+		return "redirect:/product/list";
+	}
+
+	@DeleteMapping(value="/file/{uuid}", produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> removeFile(@PathVariable("uuid")String pi_uuid){
+		return psv.removeFile(pi_uuid) > 0 ? new ResponseEntity<String>("1", HttpStatus.OK) : new ResponseEntity<String>("0", HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+
 }
